a16ebbd820b2a194d67ca6ca4d423ae99a82712d
Viacheslav <v.dovnich@gmail.com>
2018-09-27 00:08:09 +0200

    Initial + LicensePlateRecognitionController + LicensePlateRecognitionEngine + ProcessImage + ImgImport

---
+++ IOTHub/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IOTHub.Startup))]
+namespace IOTHub
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
